79d2344f629e8c74b67cae1e61b6c1df9a176f2e
Abdulbaki Dursun <eggsydev@gmail.com>
2021-09-16 18:31:42 +0300

    feat: configure meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,10 +11,53 @@ import Footer from "../components/Footer";
 import type { AppProps } from "next/app";
 
 function MarmarisPalace({ Component, pageProps }: AppProps) {
+  const meta = {
+    title: "Marmaris Palace | Wake up to a Dream",
+    description:
+      "Best place to balance your mind and body, both feeling and looking good. It is our primary mission to make these happen. You will find yourself into the untouched nature, with amazing scent of pine trees of Marmaris.",
+    image: "/meta-image.jpg",
+  };
+
   return (
     <>
       <Head>
         <title>Grand Yazıcı Club Marmaris Palace</title>
+
+        <meta
+          name="description"
+          property="description"
+          content={meta.description}
+        />
+
+        {/* Open-graph */}
+        <meta name="og:title" property="og:title" content={meta.title} />
+
+        <meta
+          name="og:description"
+          property="og:description"
+          content={meta.description}
+        />
+
+        <meta name="og:image" property="og:image" content={meta.image} />
+
+        {/* Twitter */}
+        <meta
+          name="twitter:title"
+          property="twitter:title"
+          content={meta.title}
+        />
+
+        <meta
+          name="twitter:description"
+          property="twitter:description"
+          content={meta.description}
+        />
+
+        <meta
+          name="twitter:image"
+          property="twitter:image"
+          content={meta.image}
+        />
       </Head>
 
       <Navbar />

d32fcef599fb7768520d44c7bd9e6b4d5b6c5df2
Abdulbaki Dursun <eggsydev@gmail.com>
2021-09-16 17:44:01 +0300

    feat: add social data

---
+++ src/data/social.tsx
@@ -0,0 +1,21 @@
+import { FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
+
+const Social = [
+  {
+    title: "Instagram",
+    icon: FaInstagram,
+    url: "https://instagram.com/clubmarmarispalace",
+  },
+  {
+    title: "Facebook",
+    icon: FaFacebook,
+    url: "https://facebook.com/grandyaziciclubmarmarispalace",
+  },
+  {
+    title: "Twitter",
+    icon: FaTwitter,
+    url: "https://twitter.com/marmarispalace",
+  },
+];
+
+export default Social;
